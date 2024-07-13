00b95dff8daa210bffe5734fc878b4968785f3d4
boyvotinh <LongVTQ.PTIT@gmail.com>
2024-07-13 21:03:09 +0700

    e

--- fontend/clear.js
+++ fontend/clear.js
@@ -1,6 +1,4 @@
-// Lắng nghe sự kiện click trên nút xóa dữ liệu
 document.getElementById('clearLocalStorageBtn').addEventListener('click', function() {
-    // Xóa hết dữ liệu trong localStorage
     localStorage.clear();
     alert('Đã xóa hết dữ liệu trong LocalStorage!');
 });
